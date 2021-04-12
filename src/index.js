20d20a0b387d8811c9ea9f997434d9e1deee8923
Ole Hochbruegge <ole-hochbruegge@live.nmit.ac.nz>
2021-04-13 09:43:03 +1200

    edited task2

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Task_2: Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+// Ex:  filterArr([1, 2, "a", "b"]) -> [1, 2]
+// Ex:  filterArr([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+// Note: The given array only has integers and strings.
+// Note: Numbers in the array should not repeat.
+// Note: The original order must be maintained.
