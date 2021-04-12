3eb3b33bea83f2eec2733771fa65dcbcd52aae8a
Ole Hochbruegge <ole-hochbruegge@live.nmit.ac.nz>
2021-04-13 09:44:21 +1200

    edited task3

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Task_3: Given an object containing counts of both likes and dislikes of a facebook post, return a number of count for either likes or dislikes should be displayed. This is calculated by subtracting the number of dislikes from likes.
+// Ex:  getCount({ likes: 13, dislikes: 0 }) -> 13
+// Ex:  getCount({ likes: 2, dislikes: 23 }) -> -21
+// Ex:  getCount({ likes: 100, dislikes: 100 }) -> 0
+// Note: The argument gets passed in the form of an object
+// Note: This object get assigned to the variable name "counts"
+// Note: the function body want something to operate on , which is going to be the properties of the object "counts" , so you access the properties of this object with dot notation by counts.property_1 and counts.property_2
