29b5926c8e5f02d488bd55fb243a36a9b3eacc64
Ole Hochbruegge <ole-hochbruegge@live.nmit.ac.nz>
2021-04-13 09:40:49 +1200

    edited task 1

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// ole and sharleen task 1
+// Task_1:  Create a function that returns the total of all odd numbers up to and including the given arugrment. 
+// The argument will be given as an odd number when you invoke the function. 
+// Ex:  addOddNumbers(5)  -> 9  // 1 + 3 + 5 = 9
+// Ex: addOddnumbers(13) -> 49 // 1 + 3 + 5 + 7 + 9 + 11 + 13 = 49
