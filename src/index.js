6311322db638952b54c790b430469a548d0cb749
sharleen <sharleen-quinn@live.nmit,ac.nz>
2021-04-14 13:40:41 +1200

    Commiting index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 // ole and sharleen task 1
 // Task_1:  Create a function that returns the total of all odd numbers up to and including the given argument. 
 // The argument will be given as an odd number when you invoke the function. 
-// Ex:  addOddNumbers(5)  -> 9  // 1 + 3 + 5 = 9
-// Ex: addOddnumbers(13) -> 49 // 1 + 3 + 5 + 7 + 9 + 11 + 13 = 49
-
-// function addOddNumbers() {
-//     i = 0;
-//     while [i > 0 ] {count += i; i += 2; } }
-
-//     console.log(addOddNumbers())
 
 function value(x){
     let i = 0;
     if (i > 0; i ++)
- i%2 ===0
+ i%2 === 0
  value.push(parseInt(x))
  return value
 }
 console.log(value())
+
