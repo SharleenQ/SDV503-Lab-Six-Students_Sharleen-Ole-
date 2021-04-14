0102e99533c473488b8fbdc36dac2326fde72f4b
Ole <ole-hochbruegge@live.nmit.ac.nz>
2021-04-14 12:42:42 +1200

    cleaned up task2

--- src/task2.js
+++ src/task2.js
@@ -1,28 +1,14 @@
 // Task_2: Create a function that takes an array of non-negative integers and 
 //strings and return a new array without the strings.
 
-// Ex:  filterArr([1, 2, "a", "b"]) -> [1, 2]
-// Ex:  filterArr([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
-// Note: The given array only has integers and strings.
-// Note: Numbers in the array should not repeat.
-// Note: The original order must be maintained.
-
-// integers to stings string to array.
-
-
 function filter(arr) {
   let outputArr = []
-  for (i = 0; i < arr.length; i++) {  // this loops through the array untill its equal.
+  for (i = 0; i < arr.length; i++) {  // this loops through the array until its equal.
 
-    if (typeof arr[i] !== 'string') {
+    if (typeof arr[i] !== 'string') {// if string is found string is removed
       outputArr.push(arr[i])
     } 
   }
   return (outputArr)
 }
-console.log(filter([1, 2, '0', '1', '12', 123]));
-   
-
-    // functionName();
-    // functionName('text');
-    // functionName(['text',1,4])
+console.log(filter([1, 2, '0', 'a', '12', 123]));
